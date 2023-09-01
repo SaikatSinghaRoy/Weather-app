f483a2df582356662da2037560b48d20ab82f465
SaikatSinghaRoy <saikat.singhroy@gmail.com>
2023-09-01 16:51:25 +0530

    Initial Files

---
+++ src/weather.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Weather() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+  const apiKey = 'YOUR_API_KEY';
+
+  const fetchWeatherData = async () => {
+    try {
+      // Fetch weather data using the API key and city
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={fetchWeatherData}>Get Weather</button>
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Weather;
